62e798440482a295c609f7f07863e689d88b7bdd
adreii20 <netydoma95@gmail.com>
2020-12-09 21:17:59 +0200

    Добавлен класс AndreyTestClass в модуль ManagerModule

---
+++ ManagerModule/AndreyTestClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagerModule
+{
+    public class AndreyTestClass
+    {
+        public void ToByteArr(FileInfo file)
+        {
+            FileStream fStream = new FileStream(file.FullName, FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fStream);
+            var arr = br.ReadBytes((int)file.Length);
+        }
+    }
+}
